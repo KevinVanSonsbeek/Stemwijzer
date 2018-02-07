f5b256833fd3b5c8c902610b8d0b0297182120ea
Kevin van Sonsbeek <99041317@mydavinci.nl>
2018-02-07 14:15:34 +0100

    Switched from changing page with JS (new elements adding) to switching display

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,30 @@
 var currentQuestion = 0;
 var partiesList = {};
-localStorage.clear();
+var answers = {};
+
 /**
  * Switches to screen showing questions
  */
 function changeToQuestionScreen() {
-    partiesList = {};
-    var body = document.getElementById("main");
-    body.innerHTML =
-        "<div class=\"w3-container\">\n " +
-            "<p><button class=\"w3-button w3-black w3-round w3-large\" onclick=\"previousQuestion(currentQuestion)\">Terug</button></p>\n " +
-            "<h2 class=\"w3-center\" id='subjectTitle'></h2>" +
-            "<p class=\"w3-center\" id='subjectStatement'></p>" +
-            "<div class=\"w3-bar w3-center\">" +
-                "<button class=\"w3-button w3-black w3-round w3-large\" onclick='nextQuestion(currentQuestion, \"pro\")'>Eens</button>" +
-                "<button class=\"w3-button w3-black w3-round w3-large\" onclick='nextQuestion(currentQuestion, \"ambivalent\")'>Geen van Beide</button>" +
-                "<button class=\"w3-button w3-black w3-round w3-large\" onclick='nextQuestion(currentQuestion, \"contra\")'>Oneens</button>" +
-            "</div>" +
-            "<br>" +
-            "<div class=\"w3-bar w3-center\">" +
-                "<button class=\"w3-button w3-blue w3-round w3-large\" onclick='showPartiesTable()'>Wat vinden de partijen?</button>" +
-                "<button class=\"w3-button w3-gray w3-round w3-large\" onclick='nextQuestion(currentQuestion, \"none\")'>Sla vraag over</button>" +
-            "</div>" +
-            "<br>" +
-        "</div>";
 
+    partiesList = {};
+    var startScreen = document.getElementById("startScreen");
+    var questionScreen = document.getElementById("questionScreen");
+
+    startScreen.style.display = "none";
+    questionScreen.style.display = "inline";
+
+    //Load in question
     loadQuestion(currentQuestion);
 
     for(var i = 0; i < parties.length; i++) {
         partiesList[parties[i].name] = 0;
+    }
+
+    if(answers == "") {
+        for(var i = 0; i < subjects.length; i++) {
+            answers[i] = "";
+        }
     }
 }
 
@@ -36,8 +32,15 @@ function changeToQuestionScreen() {
  * Switches to the mainscreen
  */
 function changeToMainScreen() {
-    var body = document.getElementById("main");
-    body.innerHTML = "<div class=\"w3-container w3-center\">\n <p> \n <button class=\"w3-button w3-black w3-round w3-xlarge\" onclick=\"changeToQuestionScreen()\">Start</button> \n </p> \n </div>";
+
+    var startScreen = document.getElementById("startScreen");
+    var questionScreen = document.getElementById("questionScreen");
+    var partieOpinions = document.getElementById("partieOpinons");
+
+    startScreen.style.display = "inline";
+    questionScreen.style.display = "none";
+    partieOpinions.style.display = "none";
+
 }
 
 function changeToEndResultScreen() {
@@ -47,10 +50,26 @@ function changeToEndResultScreen() {
         "<div class=\"w3-container\">" +
             "<p><button class=\"w3-button w3-black w3-round w3-large\" onclick=\"changeToQuestionScreen()\">Terug</button></p>\n " +
             "<h2>Uitslag:</h2>" +
+            "<table id='myTable' class='w3-table w3-striped w3-border'>" +
+                "<tr>" +
+                    "<th>Partij</th>" +
+                    "<th>Punten</th>" +
+                "</tr>" +
+            "</table>" +
         "</div>";
 
-    for (var c = 0; c < parties.length; c++) { //Loop through and compare partie positions
-        alertText += parties[c].name + ": " + partiesList[parties[c].name] + "\n";
+    var table = document.getElementById("myTable");
+
+    for (var i = 0; i < parties.length; i++) { //Loop through and compare partie positions
+        var row = table.insertRow(1);
+
+        // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+
+        // Add some text to the new cells:
+        cell1.innerHTML = parties[i].name;
+        cell2.innerHTML = partiesList[parties[i].name];
     }
 }
 /**
@@ -64,13 +83,15 @@ function loadQuestion(id) {
 /**
  * Loads next question
  */
-function nextQuestion(id, choice) {//options: Pro, Contra, Ambivalent, none
-    localStorage.setItem(id, choice);
+function nextQuestion(id, choice) {
+    //Save choice for question in local storage
+    answers[id] = choice;
 
-    if(currentQuestion >= (subjects.length -1)) {
+    if(currentQuestion >= (subjects.length -1)) {//If last question reached
+        console.dir(answers);
         changeToEndResultScreen();
     }
-    else {
+    else {//Load next question
         currentQuestion = id + 1;
         loadQuestion(currentQuestion);
     }
@@ -94,7 +115,21 @@ function previousQuestion(id) {
  * Shows the table with parties positions on a question
  */
 function showPartiesTable() {
-    console.log("WIP");
+    for(var i = 0; i < subjects[currentQuestion].parties.length; i++) {
+        //console.log(subjects[currentQuestion].parties[i]);
+    }
+
+    if( partieOpinons.style.display == "none") {
+        var partieOpinions = document.getElementById("partieOpinons");
+
+        partieOpinons.style.display = "inline";
+    } else {
+        var partieOpinions = document.getElementById("partieOpinons");
+
+        partieOpinons.style.display = "none";
+    }
+
+
 }
 
 /**
@@ -106,17 +141,18 @@ function calculatePartiePoints() {
 
         for (var b = 0; b < subjects[a].parties.length; b++) { //Loop through and compare partie positions
 
-            if ((localStorage[a] == "pro" && subjects[a].parties[b].position == "pro") || (localStorage[a] == "contra" && subjects[a].parties[b].position == "contra") || (localStorage[a] == "ambivalent" && subjects[a].parties[b].position == "ambivalent")) {
+            if ((answers[a] == "pro" && subjects[a].parties[b].position == "pro") || (answers[a] == "contra" && subjects[a].parties[b].position == "contra") || (answers[a] == "ambivalent" && subjects[a].parties[b].position == "ambivalent")) {
                 partiesList[subjects[a].parties[b].name] += 1;
             }
         }
 
     }
+}
 
-    var alertText = "";
-    for (var c = 0; c < parties.length; c++) { //Loop through and compare partie positions
-        alertText += parties[c].name + ": " + partiesList[parties[c].name] + "\n";
-    }
+function selectImportantQuestions() {
 
-    alert(alertText);
+}
+
+function selectPartiesToShow() {
+
 }
